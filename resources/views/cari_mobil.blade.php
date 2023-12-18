8ef9dfeb4c3c3e98937d83a4db8ef67712f2d11d
inikhairrr <121093545+inikhairrr@users.noreply.github.com>
2023-12-18 17:26:52 +0700

    Update cari_mobil.blade.php

--- resources/views/cari_mobil.blade.php
+++ resources/views/cari_mobil.blade.php
@@ -1,25 +1,46 @@
-@include('header')
-
-    <h2>Cari Mobil</h2>
+@include('header')    
 
     <form method="get" action="{{ route('mobil.searchMobil') }}">
-        <label for="keyword">Cari Mobil:</label>
-        <input type="text" name="keyword" required>
-        <button type="submit">Cari</button>
+        <label class="form-label" for="keyword">Cari Mobil:</label>
+        <input class="form-control" type="text" name="keyword" required>
+        <button class="btn btn-success" type="submit">Cari</button>
     </form>
+
+    <h2>Hasil Pencarian:</h2>  
 
     @if(isset($mobil))
         @if($mobil->count() > 0)
-            <ul>
-                @foreach($mobil as $m)
-                    <li>
-                        {{ $m->merek }} - {{ $m->model }} ({{ $m->tersedia ? 'Tersedia' : 'Tidak Tersedia' }})
-                    </li>
-                @endforeach
-            </ul>
+            
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Merek</th>
+                    <th scope="col">Model</th>
+                    <th scope="col">Status</th>            
+                </tr>
+                </thead>
+                <tbody>        
+                    @foreach($mobil as $m)
+                    <tr>
+                        <th>{{ $loop->iteration }}</th>
+                        <td>{{ $m->merek }}</td>
+                        <td>{{ $m->model }}</td>
+                        <td>
+                            ({{ $m->tersedia ? 'Tersedia' : 'Tidak Tersedia' }})
+                            @if($m->tersedia)                        
+                                    <a class="btn btn-primary" href="{{ route('mobil.formRental', ['mobilId' => $m->id]) }}">Pesan</a>                                                   
+                            @endif
+                        </td>                
+                    </tr>
+                    @endforeach      
+                </tbody>
+            </table>
+
         @else
             <p>Tidak ada hasil pencarian.</p>
         @endif
-    @endif
+    @endif      
 
+    
 @include('footer')
