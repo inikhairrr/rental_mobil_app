087c6cbc7e74b46f702505045a142edeeb36841d
inikhairrr <121093545+inikhairrr@users.noreply.github.com>
2023-12-18 17:13:52 +0700

    Update form_rental.blade.php

--- resources/views/form_rental.blade.php
+++ resources/views/form_rental.blade.php
@@ -6,17 +6,19 @@
 
 <form method="post" action="{{ route('mobil.storeRental', ['mobilId' => $mobil->id]) }}">
     @csrf
-
-    <label for="nomor_sim">Nomor SIM:</label>
-    <input type="text" name="nomor_sim" required>
-
-    <label for="tanggal_mulai">Tanggal Mulai:</label>
-    <input type="date" name="tanggal_mulai" required>
-
-    <label for="tanggal_selesai">Tanggal Selesai:</label>
-    <input type="date" name="tanggal_selesai" required>
-
-    <button type="submit">Ajukan Peminjaman</button>
+    <div class="mb-3">
+    <label class="form-label" for="nomor_sim">Nomor SIM:</label>
+    <input class="form-control" type="text" name="nomor_sim" required>
+    </div>
+    <div class="mb-3">
+    <label class="form-label" for="tanggal_mulai">Tanggal Mulai:</label>
+    <input class="form-control" type="date" name="tanggal_mulai" required>
+    </div>
+    <div class="mb-3">
+    <label class="form-label" for="tanggal_selesai">Tanggal Selesai:</label>
+    <input class="form-control" type="date" name="tanggal_selesai" required>
+    </div>
+    <button class="btn btn-primary" type="submit">Ajukan Peminjaman</button>
 </form>
 
 @include('footer')
