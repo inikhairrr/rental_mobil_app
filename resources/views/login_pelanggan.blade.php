9e440cd7679fa4b44b02f77e6fca0e52eb16eec6
inikhairrr <121093545+inikhairrr@users.noreply.github.com>
2023-12-18 17:08:01 +0700

    Update login_pelanggan.blade.php

--- resources/views/login_pelanggan.blade.php
+++ resources/views/login_pelanggan.blade.php
@@ -8,14 +8,17 @@
 
     <form method="post" action="{{ route('login_pelanggan.prosesLogin') }}">
         @csrf
-
-        <label for="nama">Nama:</label>
-        <input type="text" name="nama" required>
-
-        <label for="nomor_sim">Nomor SIM:</label>
-        <input type="text" name="nomor_sim" required>
-
-        <button type="submit">Login</button>
+        <div class="mb-3">
+        <label class="form-label" for="nama">Nama:</label>
+        <input class="form-control" type="text" name="nama" required>
+        </div>
+        <div class="mb-3">
+        <label class="form-label" for="nomor_sim">Nomor SIM:</label>
+        <input class="form-control" type="text" name="nomor_sim" required>
+        </div>
+        
+        <button class="btn btn-primary" type="submit">Login</button>
+        
     </form>
 
 @include('footer')
