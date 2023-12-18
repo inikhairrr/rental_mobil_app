81dbd1e863b3e7f0c3a6c70c56d92713e7ca3727
inikhairrr <121093545+inikhairrr@users.noreply.github.com>
2023-12-18 17:21:08 +0700

    Update tambah_mobil.blade.php

--- resources/views/tambah_mobil.blade.php
+++ resources/views/tambah_mobil.blade.php
@@ -1,21 +1,26 @@
 @include('header')
 
+    <h2>Tambah Mobil</h2>
+
     <form method="post" action="{{ route('mobil.storeMobil') }}">
         @csrf
-    
-        <label for="merek">Merek:</label>
-        <input type="text" name="merek" required>
-    
-        <label for="model">Model:</label>
-        <input type="text" name="model" required>
-    
-        <label for="nomor_plat">Nomor Plat:</label>
-        <input type="text" name="nomor_plat" required>
-    
-        <label for="tarif_sewa">Tarif Sewa per Hari:</label>
-        <input type="text" name="tarif_sewa" required>
-    
-        <button type="submit">Tambah Mobil</button>
+        <div class="mb-3">
+        <label class="form-label" for="merek">Merek:</label>
+        <input class="form-control" type="text" name="merek" required>
+        </div>
+        <div class="mb-3">
+        <label class="form-label" for="model">Model:</label>
+        <input class="form-control" type="text" name="model" required>
+        </div>
+        <div class="mb-3">
+        <label class="form-label" for="nomor_plat">Nomor Plat:</label>
+        <input class="form-control" type="text" name="nomor_plat" required>
+        </div>
+        <div class="mb-3">
+        <label class="form-label" for="tarif_sewa">Tarif Sewa per Hari:</label>
+        <input class="form-control" type="text" name="tarif_sewa" required>
+        </div>
+        <button class="btn btn-primary" type="submit">Tambah Mobil</button>
     </form>
 
 @include('footer')
