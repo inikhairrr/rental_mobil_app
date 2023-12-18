d965daeda7a362d52a649996fb4e515be6f7fe02
inikhairrr <121093545+inikhairrr@users.noreply.github.com>
2023-12-18 16:56:37 +0700

    Update daftar_rental.blade.php

--- resources/views/daftar_rental.blade.php
+++ resources/views/daftar_rental.blade.php
@@ -2,14 +2,27 @@
 
     <h2>Daftar Mobil yang Sedang Disewa</h2>
 
-<ul>
-    @foreach($rentals as $rental)
-        <li>
-            Id Mobil: {{ $rental->mobil_id }},
-            Tanggal Mulai: {{ $rental->tanggal_mulai }},
-            Tanggal Selesai: {{ $rental->tanggal_selesai }}
-        </li>
-    @endforeach
-</ul>
+<table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Id Mobil</th>
+        <th scope="col">Nomor SIM Penyewa</th>
+        <th scope="col">Tanggal Mulai</th>
+        <th scope="col">Tanggal Selesai</th>
+      </tr>
+    </thead>
+    <tbody>        
+        @foreach($rentals as $rental)
+        <tr>
+            <th>{{ $loop->iteration }}</th>
+            <td>{{ $rental->mobil_id }}</td>
+            <td>{{ $rental->user_id }}</td>
+            <td>{{ $rental->tanggal_mulai }}</td>
+            <td>{{ $rental->tanggal_selesai }}<</td>
+        </tr>
+        @endforeach      
+    </tbody>
+</table>
 
 @include('footer')
