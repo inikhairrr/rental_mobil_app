714438d7cf97ac18cc8de4b9b30c8fc482921dc1
inikhairrr <121093545+inikhairrr@users.noreply.github.com>
2023-12-18 17:18:29 +0700

    Update registrasi_pelanggan.blade.php

--- resources/views/registrasi_pelanggan.blade.php
+++ resources/views/registrasi_pelanggan.blade.php
@@ -2,20 +2,23 @@
 
     <form method="post" action="{{ route('registrasi_pelanggan.storePelanggan') }}">
         @csrf
-    
-        <label for="nama">Nama:</label>
-        <input type="text" name="nama" required>
-    
-        <label for="alamat">Alamat:</label>
-        <input type="text" name="alamat" required>
-    
-        <label for="nomor_telepon">Nomor Telepon:</label>
-        <input type="text" name="nomor_telepon" required>
-    
-        <label for="nomor_sim">Nomor SIM:</label>
-        <input type="text" name="nomor_sim" required>
-                    
-        <button type="submit">Daftar</button>
+        <div class="mb-3">
+        <label class="form-label" for="nama">Nama:</label>
+        <input class="form-control" type="text" name="nama" required>
+        </div>
+        <div class="mb-3">    
+        <label class="form-label" for="alamat">Alamat:</label>
+        <input class="form-control" type="text" name="alamat" required>
+        </div>
+        <div class="mb-3">    
+        <label class="form-label" for="nomor_telepon">Nomor Telepon:</label>
+        <input class="form-control" type="text" name="nomor_telepon" required>
+        </div>
+        <div class="mb-3">    
+        <label class="form-label" for="nomor_sim">Nomor SIM:</label>
+        <input class="form-control" type="text" name="nomor_sim" required>
+        </div>            
+        <button class="btn btn-primary" type="submit">Daftar</button>
     </form>
 
 @include('footer')
